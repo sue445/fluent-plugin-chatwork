f462b5a5d3d912fe860dad2a6f0b1c496d7bf5b4
sue445 <sue445@sue445.net>
2014-06-24 23:51:49 +0900

    Initial commit

---
+++ lib/fluent/plugin/out_chatwork.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/out_chatwork/version"
+
+module Fluent
+  module Plugin
+    module OutChatwork
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/out_chatwork/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module OutChatwork
+      VERSION = "0.0.1"
+    end
+  end
+end
