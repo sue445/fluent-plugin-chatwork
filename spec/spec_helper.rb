bb64ad36f4b205cbb13a8624ce8f362f96f6e8da
sue445 <sue445@sue445.net>
2014-06-25 00:24:33 +0900

    Setup for fluent plugin spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 #
+
+
+require 'fluent/load'
+require 'fluent/test'
+
+require 'fluent/plugin/out_chatwork'
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
